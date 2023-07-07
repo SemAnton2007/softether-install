f125de08c8fa595baa3f6cc3f8716c614c38427b
SemAnton2007 <SemAnton2007@yandex.ru>
2023-07-07 15:26:25 +0300

    some fix

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ wget -O /etc/systemd/system/vpnserver.service https://raw.githubusercontent.com/
 systemctl daemon-reload
 systemctl enable vpnserver --now
 PASSWORD=`pwgen -c 20`
+PASSWORD-USER=`pwgen -c 20`
 /opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB /CMD ServerPasswordSet $PASSWORD
+/opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB:default /CMD SecureNatEnable
+/opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB:default /CMD NatEnable
+/opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB:default /CMD DHCPEnable
 
-echo "\nPassword - $PASSWORD"
+/opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB:default /CMD UserCreate user /GROUP:none /REALNAME:none /NOTE:none
+/opt/vpnserver/vpncmd /SERVER 127.0.0.1:443 /HUB:default /CMD UserPasswordSet user /PASSWORD:$PASSWORD-USER
+
+echo "Admin Password - $PASSWORD"
+echo "User Password - $PASSWORD-USER"
